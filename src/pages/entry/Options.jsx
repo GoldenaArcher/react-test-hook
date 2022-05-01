681586db5eef551534dd8bddd4b4d886d665f6c4
GoldenaArcher <lu.han9467@gmail.com>
2022-04-30 22:39:49 -0600

    add mock api

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import ScoopOption from "./ScoopOption";
 
-const Options = ({optionType}) => {
-  return (
-    <div>Options</div>
-  )
-}
+// optionType: scoops | toppings
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
 
-export default Options
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => {
+        // TODO - do it later
+        console.log(err);
+      });
+  }, [optionType]);
+
+  // TODO - replace `null` with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} {...item} />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -2,15 +2,16 @@ import { render, screen } from "@testing-library/react";
 import Options from "../Options";
 
 describe("test scoop options function", () => {
-  test("displays image for each scoop option from server", () => {
+  test("displays image for each scoop option from server", async () => {
     render(<Options optionType="scoops" />);
 
     // find images
     // name for img is alt text
-    const scoopImages = screen.getAllByRole("img", { name: /scoop$/i });
+    const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+
     expect(scoopImages).toHaveLength(2);
 
-    const altText = scoopImages.map((element) => element.altText);
+    const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
   });
 });
